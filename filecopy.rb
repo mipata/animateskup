118181aaa048f4fb6d65c4c07d13db1879eb20f5
unknown <Miklos@.(none)>
2011-09-09 18:49:47 +0200

    Created a filecopy ruby script to automatically update the files in the PLUGINS folder of sketchup.

---
+++ filecopy.rb
@@ -0,0 +1,22 @@
+# NOTE FOR WINDOWS 7: In order for the program to be able to copy files, the permissions need to be adjusted for the
+# target folder. Just add full control to everyone for the PLUGINS folder.
+
+# First we pull in the standard API hooks.
+require 'fileutils'
+
+# Source File
+sourceFile = "stairs.rb"
+
+#Destination folder
+destFolder = "C:\\Program Files (x86)\\Google\\Google SketchUp 8\\Plugins\\"
+destFile = destFolder + sourceFile
+
+# Print out if the file exists
+print File.exists?(sourceFile),"\n"
+
+# More debugging
+print "Copying " + sourceFile + " to " + destFile + "\n"
+
+#opens/creates
+FileUtils.cp sourceFile, destFile
+
