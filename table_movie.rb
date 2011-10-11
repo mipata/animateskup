c54ad379a589c7032dd4b8b7dea8d05f278299a6
Miklos Pataky <miklosp@gmail.com>
2011-10-11 12:34:23 -0400

    modified table_movie.rb

--- table_movie.rb
+++ table_movie.rb
@@ -6,11 +6,20 @@ require 'sketchup.rb'
 Sketchup.send_action "showRubyPanel:"
 #Sketchup.send_action draw_stairs
 
+load 'transformable_ci.rb'
+class Anim
+    def nextFrame( view )
+        $mvbl.move( Geom::Vector3d.new(1, 0, 0) )
+        Sketchup.active_model().active_view().invalidate()
+        $i += 1
+        return $i < 10
+    end
+
+end
+
 # Add a menu item to launch our plugin.
 UI.menu("PlugIns").add_item("Draw table MOVIE") {
-
-  movable = MovableCI.new( ComponentInstance )
-
-  #Draw the stairs
-  draw_table
+  $i = 0
+  $mvbl = TransformableCI.new( Sketchup.active_model().selection()[0] )
+  Sketchup.active_model().active_view().animation = Anim.new()
 }
